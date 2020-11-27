4cd2ca1ebc2a0f8bf132fddae30e2efa613f136f
yinjinlin <yinjinlin_uplook@163.com>
2020-11-27 09:27:24 +0800

    xxx

---
+++ main.go
@@ -0,0 +1,14 @@
+/******************************************
+
+@Author :yinjinlin<yinjinlin_uplook@163.com>
+@Time : 2020/11/27 上午9:26
+
+*******************************************/
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("==========")
+}
+
