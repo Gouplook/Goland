256fd3217d37d9747b119913af3a4d278b17aa5a
yinjinlin <yinjinlin_uplook@163.com>
2021-04-01 13:26:58 +0800

    ..

--- utils/bigDataProcessing.go
+++ utils/bigDataProcessing.go
@@ -13,7 +13,16 @@ import (
 )
 
 func BigDataProcess(){
+	add := decimal.NewFromFloat(123.66).Add(decimal.NewFromFloat(22))
 	dec := decimal.NewFromFloat(123.66).Sub(decimal.NewFromFloat(22))
+	mul := decimal.NewFromFloat(10).Mul (decimal.NewFromFloat(22))
+	div := decimal.NewFromFloat(float64(12)).Div(decimal.NewFromFloat(float64(7)))
+	div2,b := decimal.NewFromFloat(float64(12)).Div(decimal.NewFromFloat(float64(7))).Truncate(4).Float64()
 
+	fmt.Println(add)
 	fmt.Println(dec)
+	fmt.Println(mul)
+	fmt.Println(div)
+	fmt.Println(div2,b)
+
 }
