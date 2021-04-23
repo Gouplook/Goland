bf3e11cbfe3acae20de292040666aeb33e7843e2
yinjinlin <yinjinlin_uplook@163.com>
2021-04-23 10:15:23 +0800

    ..

--- tool/advancedMap.go
+++ tool/advancedMap.go
@@ -13,15 +13,16 @@ import (
 )
 
 type CardBase struct {
-	Name   string  // 名称
-	Price  float64 // 价格
-	CardId int
-	Clicks int     // 点击量
+	Name     string  // 名称
+	Price    float64 // 价格
+	CardId   int     `mapstructure:"card_id"` // 如需要加下划线的，必须自己定义tag
+	Clicks   int     // 点击量
+	SalesNum int     // 销量 mapstructure 默认映射是小写的
 }
 
 type Card struct {
 	CardBase
-	Sales int  // 销量
+	Sales int // 销量
 }
 
 // 定义一个map
@@ -31,23 +32,30 @@ func AdvanceMap() {
 	maps[0]["name"] = "综合0"
 	maps[0]["card_id"] = 11
 	maps[0]["price"] = 100.0
+	maps[0]["salesnum"] = 1003
 	maps[1] = make(map[string]interface{})
 	maps[1]["name"] = "综合1"
 	maps[1]["card_id"] = 12
 	maps[1]["price"] = 100.1
+	maps[1]["salesnum"] = 1004
 	maps[2] = make(map[string]interface{})
 	maps[2]["name"] = "综合2"
 	maps[2]["card_id"] = 13
 	maps[2]["price"] = 200.2
+	maps[2]["salesnum"] = 1005
 
 	fmt.Println("maps=====", maps)
 	var outStruct []CardBase
 
 	_ = mapstructure.WeakDecode(maps, &outStruct)
-	//var cardMap = map[string]Card{}
 
-
-
-
+	for k, v := range outStruct {
+		fmt.Println("k = ", k, v.Name)
+		fmt.Println("k = ", k, v.CardId)
+		fmt.Println("k = ", k, v.SalesNum)
+		fmt.Println("k = ", k, v.Clicks)
+		fmt.Println("========key value =======")
+		fmt.Println(outStruct[k].Name)
+	}
 
 }
