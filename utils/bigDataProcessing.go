69af510ea811aa4269c3739ee59dfc82b51f7948
yinjinlin <yinjinlin_uplook@163.com>
2021-06-29 10:11:03 +0800

    暂存

--- utils/bigDataProcessing.go
+++ utils/bigDataProcessing.go
@@ -12,18 +12,18 @@ import (
 	"github.com/shopspring/decimal" // 大数据处理框架
 )
 
-func BigDataProcess(){
+func BigDataProcess() {
 	add := decimal.NewFromFloat(123.66).Add(decimal.NewFromFloat(22))
 	dec := decimal.NewFromFloat(123.66).Sub(decimal.NewFromFloat(22))
-	mul := decimal.NewFromFloat(10).Mul (decimal.NewFromFloat(22))
+	mul := decimal.NewFromFloat(10).Mul(decimal.NewFromFloat(22))
 	div := decimal.NewFromFloat(float64(12)).Div(decimal.NewFromFloat(float64(7)))
-	div2,b := decimal.NewFromFloat(float64(12)).Div(decimal.NewFromFloat(float64(7))).Truncate(4).Float64()
+	div2, b := decimal.NewFromFloat(float64(12)).Div(decimal.NewFromFloat(float64(7))).Truncate(4).Float64()
 
 	fmt.Println(add)
 	fmt.Println(dec)
 	fmt.Println(mul)
 	fmt.Println(div)
-	fmt.Println(div2,b)
+	fmt.Println(div2, b)
 	fmt.Println(add)
 
 	var RealPrice float64 = 10
