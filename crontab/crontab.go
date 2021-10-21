8cfbab2308f3387f9fa43c3e2987fcf08507c97f
yinjinlin <yinjinlin_uplook@163.com>
2021-10-21 16:58:42 +0800

    增加测试

---
+++ crontab/crontab.go
@@ -0,0 +1,34 @@
+/**
+ * @Author: yinjinlin
+ * @File:  crontab
+ * @Description:
+ * @Date: 2021/10/21 下午2:19
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/robfig/cron"
+	"time"
+)
+
+// 每5s启动一次
+func main(){
+	c := cron.New()
+	spec := " */5 * * * * ?"  // 秒-分-时-日-月-周【忽略】
+	_ = c.AddFunc(spec,printf)
+	c.Start()
+	select {}
+
+}
+
+func printf(){
+	local, _ := time.LoadLocation("Local")
+	localTime := time.Now().In(local).Unix()
+	localTimeStr := time.Unix(localTime,0).Format("2006-01-02-15-04-05")
+	fmt.Println(localTimeStr)
+}
+
+
+
---
+++ totest/toTested_test.go
@@ -0,0 +1,9 @@
+/**
+ * @Author: yinjinlin
+ * @File:  toTested_test
+ * @Description: 
+ * @Date: 2021/10/21 下午3:22
+ */
+
+
+package totest
