7341ba31a4bfd6bb18b2d81d188355c3f3003c3c
yinjinlin <yinjinlin_uplook@163.com>
2021-11-01 10:42:47 +0800

    添加手机、邮箱、座机电话正则检查

--- regx/goregx_test.go
+++ regx/goregx_test.go
@@ -11,7 +11,8 @@ import "testing"
 
 func TestPhoneRegx(t *testing.T) {
 	// phone := "02154377032"
-	// phone := "021-51819399019999"
-	phone :="04122-81760160"
+	// phone := "021-5181939909"
+	// 68 = 16 + 10 + 5 =
+	phone :="0411-83613910"
 	PhoneRegx(phone)
 }
